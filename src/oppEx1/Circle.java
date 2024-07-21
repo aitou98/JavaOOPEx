2162a420f12131940df0ea90867ec66583d7f60a
yassine aitoukallal <yassine.aitoukallal@gmail.com>
2024-07-21 19:18:24 +0100

    first commit

---
+++ src/oppEx1/Circle.java
@@ -0,0 +1,59 @@
+package oppEx1;
+
+public class Circle {
+	
+	private double radius=1.0;
+	private String color="red";
+	
+	
+	public Circle() {
+		
+	}
+	
+	public Circle(double radius) {
+		this.radius = radius;
+		this.color = "red";
+		
+	}
+	
+	public Circle(double radius,String color) {
+		
+		this.radius = radius;
+		this.color = color;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Cercle [radius=" + radius + ", color=" + color + "]";
+	}
+	
+	public double getArea() {
+		
+		return radius * radius * Math.PI;
+	}
+	
+    public double getCircumference() {
+		
+    	return 2.0 * radius * Math.PI;
+    }
+	
+	
+
+
+}
